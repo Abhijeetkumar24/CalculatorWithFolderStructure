4f9773d122e5763c72d331fa4fd33afe3b8f5da6
Abhijeet <abhijeet.kumar@appinventiv.com>
2023-07-07 13:15:53 +0530

    commited

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@
 import express from 'express';
 
 import calculatorRoutes from './routes/calculatorRoutes';
+require('dotenv').config();
+
 
 const app = express();
 
 app.use(express.json());
 app.use('/', calculatorRoutes);
 
-const port = 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
